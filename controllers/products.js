b18d08e4b9d87c2e65141ba3ee40a08be96e5a9b
Elina <elina0291@gmail.com>
2020-06-12 21:16:23 +0300

    Initial commit

---
+++ controllers/products.js
@@ -0,0 +1,29 @@
+//const products = [];
+const Product = require('../models/product');
+
+exports.getAddProductPage =
+    (req, res) => {
+    res.render('add-product', {
+        pageTitle: "Add New Product",
+        path: "/admin/add-product"
+    });
+}
+
+exports.postAddProducts = (req, res) => {
+    console.log(req.body.title);
+    //products.push({title: req.body.title});
+    const product = new Product(req.body.title);
+    product.saveProduct();
+    res.redirect('/');
+}
+
+exports.getProducts = (req, res) => { 
+    Product.fetchAllProducts((products) => {
+        res.render('shop', {
+        pageTitle: 'Welcome to My Shop!',
+        products: products,
+        path: '/'
+        });
+    });
+   // res.sendFile(path.join(rootDirectory, 'views', 'shop.html'));
+}
---
+++ routes/admin.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const rootDirectory = require('../utilities/path');
+const productsController = require('../controllers/products');
+const router = express.Router();
+//const products = [];
+
+router.get('/add-product', productsController.getAddroductPage);
+
+router.post('/add-product', productsController.postAddProducts);
+
+module.exports = router;
+//exports.router = router;
+//exports.products = products;
